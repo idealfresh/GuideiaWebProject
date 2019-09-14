67f92e580fb7f1723d990da31af0b29f730844c9
idealfresh <idealfresh@abv.bg>
2019-09-14 15:51:21 +0300

    update bootstrap and add Products

--- Guidea/App/Controllers/HomeController.cs
+++ Guidea/App/Controllers/HomeController.cs
@@ -34,6 +34,11 @@ namespace App.Controllers
             return View();
         }
 
+        public IActionResult Products()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
