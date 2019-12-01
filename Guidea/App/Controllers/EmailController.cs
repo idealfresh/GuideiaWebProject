fb77ec0edcb9358aeaa020ffb4c728956a5cf2c0
idealfresh <idealfresh@abv.bg>
2019-12-01 17:48:52 +0200

    Add price to products

---
+++ Guidea/App/Controllers/EmailController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using App.DTOs;
+using App.Exentions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.Controllers
+{
+    public class EmailController : Controller
+    {
+        private const string hostMail = "info.guideia@gmail.com";
+        private const string passportMail = "Guideia_001";
+         [HttpPost]
+        public IActionResult Send(EmailDto emailDto)
+        {
+            var resultString = "Email Sent Successfully!";
+            try
+            {
+                // Credentials   "info.guideia@gmail.com", "Guideia_001"
+                var credentials = new NetworkCredential(hostMail, passportMail);
+
+                // Mail message
+                var mail = new MailMessage()
+                {
+                    From = new MailAddress(emailDto.Email),
+                    Subject = "From Site Contact Form",
+                    Body = emailDto.Message.Compose(emailDto.Name,emailDto.Phone)
+                    
+                };
+
+                mail.IsBodyHtml = false;
+                //mail.To.Add(new MailAddress(emailDto.Email));
+                mail.To.Add(new MailAddress(hostMail));
+
+
+                // Smtp client
+                var client = new SmtpClient()
+                {
+                    Port = 587,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Host = "smtp.gmail.com",
+                    EnableSsl = true,
+                    Credentials = credentials
+                };
+
+                client.Send(mail);
+            }
+            catch (System.Exception e)
+            {
+                resultString = e.Message;
+            }
+
+            return RedirectToAction("Contact", "Home", resultString);
+        }
+    }
+}
---
+++ Guidea/App/Exentions/EmailComposer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Exentions
+{
+    public static class EmailComposer
+    {
+        public static string Compose(this string msg,string name,string phoneNumber)
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Name: {name}");
+            sb.AppendLine($"Phone Number: {phoneNumber}");
+            sb.AppendLine();
+            sb.AppendLine(msg);
+
+            return sb.ToString();
+        }
+    }
+}
