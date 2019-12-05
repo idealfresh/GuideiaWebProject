f67b9756e286e7b798bff922fcbef48e1715915d
idealfresh <idealfresh@abv.bg>
2019-12-05 16:52:47 +0200

    Before changing products group

---
+++ Guidea/App/Controllers/BatteryTypeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.Controllers
+{
+    public class BatteryTypeController : Controller
+    {
+        public IActionResult BatteryCell()
+        {
+            return View();
+        }
+        public IActionResult BatteryPack()
+        {
+            return View();
+        }
+        public IActionResult BatteryRepair()
+        {
+            return View();
+        }
+    }
+}
