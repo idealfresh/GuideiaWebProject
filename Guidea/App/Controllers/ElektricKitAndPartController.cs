d581b5a7210b81d89b04534c7f9e865bc2428324
idealfresh <idealfresh@abv.bg>
2019-12-12 15:43:33 +0200

    Working on magnets one view two parts. Changing Product View

---
+++ Guidea/App/Controllers/ElektricKitAndPartController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.Controllers
+{
+    public class ElektricKitAndPartController : Controller
+    {
+        public IActionResult FrontKit()
+        {
+            return View();
+        }
+        public IActionResult RearKit()
+        {
+            return View();
+        }
+        public IActionResult MiddleKit()
+        {
+            return View();
+        }
+    }
+}
